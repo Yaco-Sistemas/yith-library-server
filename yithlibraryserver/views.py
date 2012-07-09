1856b20abc52565d2bd217d3fa55ff07907b33b4
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-09 12:50:34 +0200

    Send the output in JSON

--- yithlibraryserver/views.py
+++ yithlibraryserver/views.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config, view_defaults
 from pyramid.response import Response
 
 
-@view_defaults(route_name='password_collection_view')
+@view_defaults(route_name='password_collection_view', renderer='json')
 class PasswordCollectionRESTView(object):
 
     def __init__(self, request):
@@ -10,14 +10,32 @@ class PasswordCollectionRESTView(object):
 
     @view_config(request_method='GET')
     def get(self):
-        return Response('get password collection')
+        passwords = [
+            {
+                'secret': '1',
+                'service': 'foo',
+                'account': 'pepito',
+                'expiration': 0,
+                'notes': '',
+                'tags': [],
+                },
+            {
+                'secret': '2',
+                'service': 'bar',
+                'account': 'susanita',
+                'expiration': 0,
+                'notes': '',
+                'tags': [],
+                },
+            ]
+        return passwords
 
     @view_config(request_method='POST')
     def post(self):
         return Response('post password collection')
 
 
-@view_defaults(route_name='password_view')
+@view_defaults(route_name='password_view', renderer='json')
 class PasswordRESTView(object):
 
     def __init__(self, request):
