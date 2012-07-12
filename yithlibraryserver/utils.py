e74bbf3a75f088ea27b0a1fb8d8509f1c4316529
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-12 21:53:18 +0200

    Convert all possible values, not just the _id

--- yithlibraryserver/utils.py
+++ yithlibraryserver/utils.py
@@ -1,8 +1,17 @@
+import bson
+
+
 def jsonable(mongo_object):
     """Makes a mongodb object jsonable.
 
     This function modifies the argument in place.
     """
-    # the _id attribute is not serializable to JSON
-    mongo_object['_id'] = str(mongo_object['_id'])
-    return mongo_object
+    new_obj = {}
+    for key, value in mongo_object.items():
+        if isinstance(value, bson.ObjectId):
+            # bson.ObjectId is not JSON serializable
+            new_obj[key] = str(value)
+        else:
+            new_obj[key] = value
+    return new_obj
+
