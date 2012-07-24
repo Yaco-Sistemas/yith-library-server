f1cb71f6647a843f519606da4a8f652fd3f8a172
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-24 20:55:15 +0200

    Add some logging calls to the setting reading

--- yithlibraryserver/config.py
+++ yithlibraryserver/config.py
@@ -1,9 +1,17 @@
+import logging
 import os
+
+
+log = logging.getLogger(__name__)
 
 
 def read_setting_from_env(settings, key, default=None):
     env_variable = key.upper()
     if env_variable in os.environ:
+        log.debug('Setting %s found in the environment: %s' %
+                  (key, os.environ[env_variable]))
         return os.environ[env_variable]
     else:
+        log.debug('Looking for setting %s in the selected .ini file: %s' %
+                  (key, settings.get(key, default)))
         return settings.get(key, default)
