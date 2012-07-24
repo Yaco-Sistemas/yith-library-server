b62f2c2ef9f37730f8df1f7f364b5f97f55cd81d
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-24 20:49:49 +0200

    Environment variables should have more priority than .ini files

--- yithlibraryserver/config.py
+++ yithlibraryserver/config.py
@@ -2,8 +2,8 @@ import os
 
 
 def read_setting_from_env(settings, key, default=None):
-    if key in settings and settings:
-        return settings.get(key, default)
+    env_variable = key.upper()
+    if env_variable in os.environ:
+        return os.environ[env_variable]
     else:
-        env_variable = key.upper()
-        return os.environ.get(env_variable, default)
+        return settings.get(key, default)
