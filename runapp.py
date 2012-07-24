76f77023095b068402553b878a65f0566d8add81
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-24 20:45:41 +0200

    Enable logs when run in Heroku

--- runapp.py
+++ runapp.py
@@ -1,10 +1,12 @@
 import os
 
 from paste.deploy import loadapp
+from pyramid.paster import setup_logging
 from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    setup_logging('production.ini')
     app = loadapp('config:production.ini', relative_to='.')
 
     serve(app, host='0.0.0.0', port=port)
