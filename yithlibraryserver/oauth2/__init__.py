3ef0b26c92e698fab191aa577f4806d691a8d26a
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-27 00:17:39 +0200

    Customize some deform templates

--- yithlibraryserver/oauth2/__init__.py
+++ yithlibraryserver/oauth2/__init__.py
@@ -1,4 +1,15 @@
+from pkg_resources import resource_filename
+from deform import Form
+
+from pyramid.path import AssetResolver
+
 def includeme(config):
+    # override deform templates
+    deform_templates = resource_filename('deform', 'templates')
+    resolver = AssetResolver('yithlibraryserver.oauth2')
+    search_path = (resolver.resolve('templates').abspath(), deform_templates)
+    Form.set_zpt_renderer(search_path)
+
     config.add_route('oauth2_applications', '/oauth2/applications')
     config.add_route('oauth2_application_new', '/oauth2/applications/new')
     config.add_route('oauth2_application_view', '/oauth2/applications/{app}')
