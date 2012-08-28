a1605f90246bbc63edbfc2b857dfd1de7ce11d59
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-08-28 19:28:41 +0200

    Use the ax extension to grab some user attributes

--- yithlibraryserver/google/views.py
+++ yithlibraryserver/google/views.py
@@ -1,6 +1,7 @@
 from pyramid.httpexceptions import HTTPFound
 
 from openid.consumer import consumer
+from openid.extensions import ax
 from openid.store.filestore import FileOpenIDStore
 
 from yithlibraryserver.compat import urlparse
@@ -11,12 +12,26 @@ def _get_consumer(request):
     return consumer.Consumer(request.session, store)
 
 
+AX_ATTRS = {
+    'firstname': 'http://axschema.org/namePerson/first',
+    'lastname': 'http://axschema.org/namePerson/last',
+    'email': 'http://axschema.org/contact/email',
+}
+
+
 def google_login(request):
     settings = request.registry.settings
     openid_url = settings['google_openid_url']
 
     consumer = _get_consumer(request)
     auth_req = consumer.begin(openid_url)
+
+    fetch_req = ax.FetchRequest()
+    fetch_req.add(ax.AttrInfo(AX_ATTRS['firstname'], required=True))
+    fetch_req.add(ax.AttrInfo(AX_ATTRS['lastname'], required=True))
+    fetch_req.add(ax.AttrInfo(AX_ATTRS['email'], required=True))
+
+    auth_req.addExtension(fetch_req)
 
     return_to = request.route_url('google_callback')
     parts = urlparse.urlparse(return_to)
@@ -29,6 +44,10 @@ def google_callback(request):
     con = _get_consumer(request)
     info = con.complete(request.GET, request.url)
     if info.status == consumer.SUCCESS:
+        fr = ax.FetchResponse.fromSuccessResponse(info)
+        print fr.getSingle(AX_ATTRS['firstname'])
+        print fr.getSingle(AX_ATTRS['lastname'])
+        print fr.getSingle(AX_ATTRS['email'])
         return info.getDisplayIdentifier()
     elif info.status == consumer.CANCEL:
         return 'canceled'
