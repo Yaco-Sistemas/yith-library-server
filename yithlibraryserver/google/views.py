07232df1cb9c5c29f9a451d002d00ab704a7e57e
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-08-28 19:40:07 +0200

    Register or login the user with Google attributes

--- yithlibraryserver/google/views.py
+++ yithlibraryserver/google/views.py
@@ -1,10 +1,12 @@
 from pyramid.httpexceptions import HTTPFound
+from pyramid.security import remember
 
 from openid.consumer import consumer
 from openid.extensions import ax
 from openid.store.filestore import FileOpenIDStore
 
 from yithlibraryserver.compat import urlparse
+from yithlibraryserver.user.utils import update_user
 
 def _get_consumer(request):
     settings = request.registry.settings
@@ -13,8 +15,8 @@ def _get_consumer(request):
 
 
 AX_ATTRS = {
-    'firstname': 'http://axschema.org/namePerson/first',
-    'lastname': 'http://axschema.org/namePerson/last',
+    'first_name': 'http://axschema.org/namePerson/first',
+    'last_name': 'http://axschema.org/namePerson/last',
     'email': 'http://axschema.org/contact/email',
 }
 
@@ -27,8 +29,8 @@ def google_login(request):
     auth_req = consumer.begin(openid_url)
 
     fetch_req = ax.FetchRequest()
-    fetch_req.add(ax.AttrInfo(AX_ATTRS['firstname'], required=True))
-    fetch_req.add(ax.AttrInfo(AX_ATTRS['lastname'], required=True))
+    fetch_req.add(ax.AttrInfo(AX_ATTRS['first_name'], required=True))
+    fetch_req.add(ax.AttrInfo(AX_ATTRS['last_name'], required=True))
     fetch_req.add(ax.AttrInfo(AX_ATTRS['email'], required=True))
 
     auth_req.addExtension(fetch_req)
@@ -36,6 +38,9 @@ def google_login(request):
     return_to = request.route_url('google_callback')
     parts = urlparse.urlparse(return_to)
     realm = '%s://%s/' % (parts.scheme, parts.netloc)
+
+    if 'next_url' in request.params:
+        request.session['next_url'] = request.params['next_url']
 
     return HTTPFound(location=auth_req.redirectURL(realm, return_to=return_to))
 
@@ -45,10 +50,31 @@ def google_callback(request):
     info = con.complete(request.GET, request.url)
     if info.status == consumer.SUCCESS:
         fr = ax.FetchResponse.fromSuccessResponse(info)
-        print fr.getSingle(AX_ATTRS['firstname'])
-        print fr.getSingle(AX_ATTRS['lastname'])
-        print fr.getSingle(AX_ATTRS['email'])
-        return info.getDisplayIdentifier()
+        user_id = info.getDisplayIdentifier()
+
+        if 'next_url' in request.session:
+            next_url = request.session['next_url']
+            del request.session['next_url']
+        else:
+            next_url = request.route_path('home')
+
+        user = request.db.users.find_one({'google_id': user_id})
+        info = {
+                'provider': 'google',
+                'google_id': user_id,
+                'first_name': fr.getSingle(AX_ATTRS['first_name']),
+                'last_name': fr.getSingle(AX_ATTRS['last_name']),
+                'email': fr.getSingle(AX_ATTRS['email']),
+            }
+        if user is None:
+            request.session['user_info'] = info
+            request.session['next_url'] = next_url
+            return HTTPFound(location=request.route_path('register_new_user'))
+        else:
+            update_user(request.db, user, info)
+            remember_headers = remember(request, str(user['_id']))
+            return HTTPFound(location=next_url, headers=remember_headers)
+
     elif info.status == consumer.CANCEL:
         return 'canceled'
     elif info.status == consumer.FAILURE:
