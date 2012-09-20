115b440bc6e3736f29849fc0df97fd436b14dbed
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-09-20 22:10:00 +0200

    Returning the exception makes this function easier to test

--- yithlibraryserver/oauth2/client.py
+++ yithlibraryserver/oauth2/client.py
@@ -95,6 +95,6 @@ def get_user_info(info_uri, access_token):
     response = requests.get(info_uri, headers=headers)
 
     if response.status_code != 200:
-        raise HTTPUnauthorized(response.text)
+        return HTTPUnauthorized(response.text)
 
     return response.json
