b3f982815f65f18d777775e47fb645008522adcc
Lorenzo Gil <lgs@yaco.es>
2012-10-20 13:54:46 +0200

    By default, serve everything under https. The scheme will be use to generate public URLs from the app

--- runapp.py
+++ runapp.py
@@ -26,7 +26,8 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    scheme = os.environ.get("SCHEME", "https")
     setup_logging('production.ini')
     app = loadapp('config:production.ini', relative_to='.')
 
-    serve(app, host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port, scheme=scheme)
