efe351f928efe54018f975571b338e822791ac17
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-11-11 23:05:33 +0100

    Add more information in the terms of service

--- yithlibraryserver/views.py
+++ yithlibraryserver/views.py
@@ -24,3 +24,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/home.pt')
 def home(request):
     return {}
+
+
+@view_config(route_name='tos', renderer='templates/tos.pt')
+def tos(request):
+    return {}
