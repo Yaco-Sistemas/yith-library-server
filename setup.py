2fd520bed237a95afed8cd92cc1d360d23c7af37
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-11-25 20:45:35 +0100

    WebTest requires waitress 0.8.1 and requests 0.14.0 so I downgrade those packages to avoid conflicts when installing our test requirements

--- setup.py
+++ setup.py
@@ -34,15 +34,15 @@ requires = [
     'pyramid_debugtoolbar==1.0.2',
     'pyramid_mailer==0.9',
     'pyramid_tm==0.5',
-    'requests==0.14.1',
-    'waitress==0.8.2',
+    'requests==0.14.0',
+    'waitress==0.8.1',
     ]
 
 test_requires = [
-    'WebTest==1.4.2',
-    'mock==1.0.0',
-    'nose==1.2.1',
-    'coverage==3.5.2',
+    'WebTest',
+    'mock',
+    'nose',
+    'coverage',
     ]
 
 setup(name='yith-library-server',
