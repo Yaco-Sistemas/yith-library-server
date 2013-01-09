df163832af6218558d8cbb0af9caabb5400da8b0
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-01-10 00:20:55 +0100

    Add tests for the deform translator

--- yithlibraryserver/tests/test_i18n.py
+++ yithlibraryserver/tests/test_i18n.py
@@ -16,10 +16,26 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Yith Library Server.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
 from webtest import TestRequest
 
-from yithlibraryserver.i18n import locale_negotiator
+from pyramid import testing
+
+from yithlibraryserver.i18n import deform_translator, locale_negotiator
 from yithlibraryserver.testing import TestCase
+
+
+class DeformTranslatorTests(unittest.TestCase):
+
+    def setUp(self):
+        request = testing.DummyRequest()
+        testing.setUp(request=request)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_deform_translator(self):
+        self.assertEqual('foo', deform_translator('foo'))
 
 
 class LocaleNegotiatorTests(TestCase):
