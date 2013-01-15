71f590d57fcfc2d48f50255dd4d34b0a4c416cf1
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-01-15 20:56:28 +0100

    Fix Heroku deployment

--- runapp.py
+++ runapp.py
@@ -27,7 +27,7 @@ from waitress import serve
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     scheme = os.environ.get("SCHEME", "https")
-    setup_logging('production.ini')
-    app = loadapp('config:production.ini', relative_to='.')
+    setup_logging('yithlibraryserver/config-templates/production.ini')
+    app = loadapp('config:production.ini', relative_to='yithlibraryserver/config-templates')
 
     serve(app, host='0.0.0.0', port=port, url_scheme=scheme)
